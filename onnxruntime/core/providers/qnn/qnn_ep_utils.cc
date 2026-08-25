d88752259ee6102dd97809b2b9a3b80ab721ae6f
qti-mbadnara <mbadnara@qti.qualcomm.com>
2026-08-24 17:29:48 -0700

    [QNN EP] Add support for ArrayFeatureExtractor Op (#710)

--- onnxruntime/core/providers/qnn/qnn_ep_utils.cc
+++ onnxruntime/core/providers/qnn/qnn_ep_utils.cc
@@ -1836,7 +1836,7 @@ std::vector<OrtNodeGroup> OrtSelectorManager::GetOrtQDQSelections(const OrtGraph
 
     // Check domain (similar to the GraphViewer version)
     std::string domain_str(domain);
-    if (domain_str != kOnnxDomain && domain_str != kMSInternalNHWCDomain && domain_str != kMSDomain) {
+    if (domain_str != kOnnxDomain && domain_str != kMSInternalNHWCDomain && domain_str != kMSDomain && domain_str != kMLOnnxDomain) {
       continue;
     }
 
